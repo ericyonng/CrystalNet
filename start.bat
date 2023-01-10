1e9514e0840a7e7890b38284fa8e02071579e5d8
ericyonng <120453674@qq.com>
2023-01-10 22:12:52 +0800

    起服，停服脚本

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+
+SET CUR_PATH=%~dp0
+echo For now supported Visual Studio versions:
+echo    vs2015
+echo    vs2017
+echo    vs2019
+echo    vs2022
+set /p choose=Please input:
+
+cd %CUR_PATH%/output/%choose% && start testsuit.exe
+
