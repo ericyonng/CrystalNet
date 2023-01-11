1c3362679bde10f44b57dc492654eb0a79971d73
ERICYONNG\EricYonng <120453674@qq.com>
2023-01-12 02:31:43 +0800

    脚本优化

--- runfirstly_scripts.sh
+++ runfirstly_scripts.sh
@@ -1,32 +1,3 @@
-# @file linuxmakefile_build.sh
-###
- #  MIT License
- #  
- #  Copyright (c) 2020 ericyonng<120453674@qq.com>
- #  
- #  Permission is hereby granted, free of charge, to any person obtaining a copy
- #  of this software and associated documentation files (the "Software"), to deal
- #  in the Software without restriction, including without limitation the rights
- #  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- #  copies of the Software, and to permit persons to whom the Software is
- #  furnished to do so, subject to the following conditions:
- #  
- #  The above copyright notice and this permission notice shall be included in all
- #  copies or substantial portions of the Software.
- #  
- #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- #  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- #  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- #  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- #  SOFTWARE.
- # 
- # @Date: 2022-09-03 04:44:16
- # @Author: Eric Yonng
- # @Description: 
-###
-
 # @author EricYonng<120453674@qq.com>
 # @brief 3rd scripts
 #!/usr/bin/env bash
