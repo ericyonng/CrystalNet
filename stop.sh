47cd2feb4a09d608440b720fc05b2376f8ac1751
ERICYONNG\EricYonng <120453674@qq.com>
2023-01-12 02:12:03 +0800

    优化关闭脚本

--- stop.sh
+++ stop.sh
@@ -14,30 +14,12 @@ fi
 
 echo "will stop process with GREP_FLAG:${GREP_FLAG}"
 
-PID_LIST="$(ps -aux |grep ${GREP_FLAG} | sed 's/^[^ ]* //' | sed 's/^ *//' | sed 's/ .*$//')"
-
-FILTER_LIST="$(ps -aux |grep ${GREP_FLAG} | grep stop | sed 's/^[^ ]* //' | sed 's/^ *//' | sed 's/ .*$//')"
+PID_LIST="$(ps -aux |grep ${GREP_FLAG} | sed '/grep/d' | sed '/stop/d' | sed 's/^[^ ]* //' | sed 's/^ *//' | sed 's/ .*$//')"
 
 echo "will stop PID_LIST:${PID_LIST}"
-echo "FILTER_LIST:${FILTER_LIST}"
 
 for pid in $PID_LIST
 do
-    IS_FILTER_PID=0
-    for filter_pid in $FILTER_LIST
-    do
-        if [ $pid -eq $filter_pid ]
-        then
-            IS_FILTER_PID=1
-            break
-        fi
-    done
-
-    if [ $pid -eq 1 ]
-    then
-        continue
-    fi
-
     echo "stop process $pid"
     kill 2 $pid
 done
