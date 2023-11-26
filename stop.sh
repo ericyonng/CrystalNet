01e666f084fd4cd56c241a3c13f4e42b3ccb9a9b
ERICYONNG\EricYonng <120453674@qq.com>
2023-11-27 00:05:50 +0800

    更新脚本
    Former-commit-id: c3f1caf8e25e7c5331137abfa82c25d9c4458603

--- stop.sh
+++ stop.sh
@@ -5,35 +5,38 @@
 # 路径
 SCRIPT_PATH="$(cd $(dirname $0); pwd)"
 
-GREP_FLAG="CrystalNet"
-if [ $1 ]
-then
-    echo "param 1:$1"
-    GREP_FLAG=$1
-fi
+# GREP_FLAG="CrystalNet"
+# if [ $1 ]
+# then
+#     echo "param 1:$1"
+#     GREP_FLAG=$1
+# fi
 
-echo "will stop process with GREP_FLAG:${GREP_FLAG}"
+# echo "will stop process with GREP_FLAG:${GREP_FLAG}"
 
-PID_LIST="$(ps -aux |grep ${GREP_FLAG} | sed '/grep/d' | sed '/stop/d' | sed 's/^[^ ]* //' | sed 's/^ *//' | sed 's/ .*$//')"
+# PID_LIST="$(ps -aux |grep ${GREP_FLAG} | sed '/grep/d' | sed '/stop/d' | sed 's/^[^ ]* //' | sed 's/^ *//' | sed 's/ .*$//')"
 
-echo "will stop PID_LIST:${PID_LIST}"
+# echo "will stop PID_LIST:${PID_LIST}"
 
-for pid in $PID_LIST
-do
-    echo "stop process $pid"
-    kill 2 $pid
-done
+# for pid in $PID_LIST
+# do
+#     echo "stop process $pid"
+#     kill 2 $pid
+# done
 
-for pid in $PID_LIST
-do
-    echo "wait pid:${pid} close..."
-    while [ -n "$(ps -p $pid | sed '1d')" ]
-    do
-        echo "wait pid:${pid} close..."
-        sleep 1
-    done
+# for pid in $PID_LIST
+# do
+#     echo "wait pid:${pid} close..."
+#     while [ -n "$(ps -p $pid | sed '1d')" ]
+#     do
+#         echo "wait pid:${pid} close..."
+#         sleep 1
+#     done
 
-    echo "pid:${pid}, has closed."
-done
+#     echo "pid:${pid}, has closed."
+# done
 
-echo "all process has closed."
+# echo "all process has closed."
+
+${SCRIPT_PATH}/tools/CloseProcess/CloseProcess testsuit is_waiting_close=1
+
