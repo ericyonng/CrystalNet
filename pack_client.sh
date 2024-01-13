b9926044b8e86874dbe095c72cfa6d6047f225c2
ERICYONNG\EricYonng <120453674@qq.com>
2024-01-13 18:20:59 +0800

    添加打包脚本

---
+++ pack_client.sh
@@ -0,0 +1,25 @@
+# @brief pack testsuit...
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$(cd $(dirname $0); pwd)"
+
+TAR_NAME=$1
+if [ -z ${TAR_NAME} ]
+then
+    TAR_NAME=client
+fi
+
+sh ${SCRIPT_PATH}/runfirstly_scripts.sh
+sh ${SCRIPT_PATH}/linuxmakefile_build.sh
+cd ${SCRIPT_PATH}/build/gmake/ && make client config=release
+sh ${SCRIPT_PATH}/install.sh release
+
+cp -Rf ${SCRIPT_PATH}/tools ${SCRIPT_PATH}/output/gmake/
+cp -Rf ${SCRIPT_PATH}/start.sh ${SCRIPT_PATH}/output/gmake/
+cp -Rf ${SCRIPT_PATH}/stop.sh ${SCRIPT_PATH}/output/gmake/
+cp -Rf ${SCRIPT_PATH}/linux_run.sh ${SCRIPT_PATH}/output/gmake/
+
+cd ${SCRIPT_PATH}
+rm -f ${SCRIPT_PATH}/output/gmake/${TAR_NAME}.tgz
+cd ${SCRIPT_PATH}/output/gmake && tar -zcvf ${TAR_NAME}.tgz ./client ./Cfgs/* ./ini/* ./libmysqlclient.so ./linux_run.sh ./start.sh ./stop.sh ./tools
+
