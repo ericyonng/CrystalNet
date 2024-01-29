aba85a0bc85ad018f724242c55ed380d6a0f7dc4
ericyonng <120453674@qq.com>
2024-01-29 15:36:04 +0800

    打包脚本优化

--- pack_testsuit.sh
+++ pack_testsuit.sh
@@ -3,6 +3,12 @@
 
 SCRIPT_PATH="$(cd $(dirname $0); pwd)"
 
+RUN_NAME=$2
+if [ -z ${RUN_NAME} ]
+then
+    RUN_NAME=testsuit
+fi
+
 TAR_NAME=$1
 if [ -z ${TAR_NAME} ]
 then
@@ -18,6 +24,11 @@ cp -Rf ${SCRIPT_PATH}/tools ${SCRIPT_PATH}/output/gmake/
 cp -Rf ${SCRIPT_PATH}/start.sh ${SCRIPT_PATH}/output/gmake/
 cp -Rf ${SCRIPT_PATH}/stop.sh ${SCRIPT_PATH}/output/gmake/
 cp -Rf ${SCRIPT_PATH}/linux_run.sh ${SCRIPT_PATH}/output/gmake/
+
+sudo sed -i 's/^RUN_PATH=.*/RUN_PATH=${SCRIPT_PATH}/' ${SCRIPT_PATH}/output/gmake/linux_run.sh
+sudo sed -i "s/^RUN_NAME=\".*/RUN_NAME=\"${RUN_NAME}\"/" ${SCRIPT_PATH}/output/gmake/linux_run.sh
+sudo sed -i "s/linux_run\.sh.*/linux_run\.sh ${RUN_NAME} \$1/" ${SCRIPT_PATH}/output/gmake/start.sh
+sudo sed -i "s/CloseProcess.*is_waiting_close.*/CloseProcess ${RUN_NAME} is_waiting_close=1/" ${SCRIPT_PATH}/output/gmake/stop.sh
 
 cd ${SCRIPT_PATH}
 rm -f ${SCRIPT_PATH}/output/gmake/${TAR_NAME}.tgz
