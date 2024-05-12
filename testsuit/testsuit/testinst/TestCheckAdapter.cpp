8538bdca0d348138adf8bf7f66b134cb2580e1bc
ericyonng <120453674@qq.com>
2024-05-12 18:32:06 +0800

    更新工具

--- testsuit/testsuit/testinst/TestCheckAdapter.cpp
+++ testsuit/testsuit/testinst/TestCheckAdapter.cpp
@@ -34,36 +34,81 @@ void TestCheckAdapter::Run()
     auto checkMtLibStream = KERNEL_NS::CheckTypeSerializable<KERNEL_NS::LibStream<KERNEL_NS::_Build::MT>, KERNEL_NS::_Build::MT>::value;
     auto checkTlLibStream = KERNEL_NS::CheckTypeSerializable<KERNEL_NS::LibStream<KERNEL_NS::_Build::TL>, KERNEL_NS::_Build::TL>::value;
     auto checkCurrentType = KERNEL_NS::CheckTypeSerializable<TestCheckAdapter, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkCurrentType);
     auto checkCurrentType2 = KERNEL_NS::CheckTypeSerializable<TestCheckAdapter, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkCurrentType2);
+
     auto checkInt32 = KERNEL_NS::CheckTypeSerializable<Int32, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkInt32);
+
     auto checkInt32TL = KERNEL_NS::CheckTypeSerializable<Int32, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkInt32TL);
+
     auto checkInt32Ptr = KERNEL_NS::CheckTypeSerializable<Int32 *, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkInt32Ptr);
+
     auto checkInt32PtrTL = KERNEL_NS::CheckTypeSerializable<Int32 *, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkInt32PtrTL);
+
     auto checkInt32ArrTL = KERNEL_NS::CheckTypeSerializable<Int32 [], KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkInt32ArrTL);
+
     auto checkInt32ArrMT = KERNEL_NS::CheckTypeSerializable<Int32 [], KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkInt32ArrMT);
 
     auto checkDeserializeMtLibStream = KERNEL_NS::CheckTypeDeserializable<KERNEL_NS::LibStream<KERNEL_NS::_Build::MT>, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkDeserializeMtLibStream);
+
     auto checkDeserializeTlLibStream = KERNEL_NS::CheckTypeDeserializable<KERNEL_NS::LibStream<KERNEL_NS::_Build::TL>, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkDeserializeTlLibStream);
+
     auto checkDeserializeCurrentType = KERNEL_NS::CheckTypeDeserializable<TestCheckAdapter, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkDeserializeCurrentType);
+
     auto checkDeserializeCurrentType2 = KERNEL_NS::CheckTypeDeserializable<TestCheckAdapter, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkDeserializeCurrentType2);
+
     auto checkDeserializeInt32 = KERNEL_NS::CheckTypeDeserializable<Int32, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkDeserializeInt32);
+
     auto checkDeserializeInt32TL = KERNEL_NS::CheckTypeDeserializable<Int32, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkDeserializeInt32TL);
+
     auto checkDeserializeInt32Ptr = KERNEL_NS::CheckTypeDeserializable<Int32 *, KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkDeserializeInt32Ptr);
+
     auto checkDeserializeInt32PtrTL = KERNEL_NS::CheckTypeDeserializable<Int32 *, KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkDeserializeInt32PtrTL);
+
     auto checkDeserializeInt32ArrTL = KERNEL_NS::CheckTypeDeserializable<Int32 [], KERNEL_NS::_Build::TL>::value;
+    UNUSED(checkDeserializeInt32ArrTL);
+
     auto checkDeserializeInt32ArrMT = KERNEL_NS::CheckTypeDeserializable<Int32 [], KERNEL_NS::_Build::MT>::value;
+    UNUSED(checkDeserializeInt32ArrMT);
 
     auto checkSerializableMtLibStream = KERNEL_NS::CheckSerializable<KERNEL_NS::LibStream<KERNEL_NS::_Build::MT>>::value;
+    UNUSED(checkSerializableMtLibStream);
+
     auto checkSerializableTLLibStream = KERNEL_NS::CheckSerializable<KERNEL_NS::LibStream<KERNEL_NS::_Build::TL>>::value;
+    UNUSED(checkSerializableTLLibStream);
+
     auto checkSerializableCurrentType = KERNEL_NS::CheckSerializable<TestCheckAdapter>::value;
+    UNUSED(checkSerializableCurrentType);
+
     auto checkSerializableInt32 = KERNEL_NS::CheckSerializable<Int32>::value;
+    UNUSED(checkSerializableInt32);
+
     auto checkSerializableInt32Ptr = KERNEL_NS::CheckSerializable<Int32 *>::value;
+    UNUSED(checkSerializableInt32Ptr);
+
     auto checkSerializableInt32Arr = KERNEL_NS::CheckSerializable<Int32 []>::value;
+    UNUSED(checkSerializableInt32Arr);
 
     auto isMultiCheckSerializable = KERNEL_NS::CheckAdapterRule::GetAndResult((bool)(KERNEL_NS::CheckSerializable<Int32>::value), (bool)(KERNEL_NS::CheckSerializable<Int32 *>::value));
     isMultiCheckSerializable = KERNEL_NS::CheckAdapterRule::GetAndResult((bool)(KERNEL_NS::CheckSerializable<Int32>::value), (bool)(KERNEL_NS::CheckSerializable<Int32 []>::value));
     isMultiCheckSerializable = KERNEL_NS::CheckAdapterRule::GetOrResult((bool)(KERNEL_NS::CheckSerializable<Int32>::value), (bool)(KERNEL_NS::CheckSerializable<Int32>::value));
     isMultiCheckSerializable = KERNEL_NS::CheckAdapterRule::GetOrResult((bool)(KERNEL_NS::CheckSerializable<TestCheckAdapter>::value), (bool)(KERNEL_NS::CheckSerializable<Int32[]>::value));
+    UNUSED(isMultiCheckSerializable);
 
     g_Log->Info(LOGFMT_NON_OBJ_TAG(TestCheckAdapter, "checkMtLibStream:%d, checkTlLibStream:%d")
     , checkMtLibStream, checkTlLibStream);
--- testsuit/testsuit/testinst/TestMessageQueue.cpp
+++ testsuit/testsuit/testinst/TestMessageQueue.cpp
@@ -75,222 +75,223 @@ public:
 POOL_CREATE_OBJ_DEFAULT_IMPL(TestMqBlock);
 
 // mid:4247kqps, average:4246kqps
-static void Generator(KERNEL_NS::LibThreadPool *t)
-{
-    while (!t->IsDestroy())
-    {
-        g_Queue->PushBack(TestMqBlock::New_TestMqBlock());
-        ++g_curGenCount;
-    }
-}
+// static void Generator(KERNEL_NS::LibThreadPool *t)
+// {
+//     while (!t->IsDestroy())
+//     {
+//         g_Queue->PushBack(TestMqBlock::New_TestMqBlock());
+//         ++g_curGenCount;
+//     }
+// }
 
-// mid:3745kqps, average:3759kqps
-static void Consumer(KERNEL_NS::LibThreadPool *t)
-{
-    TestMqBlock *newEv = NULL;
-    while (!t->IsDestroy())
-    {
-        newEv = NULL;
-        if(g_Queue->PopFront(newEv))
-        {
-            newEv->Release();
-            ++g_curMsgConsume;
-        }
-    }
-}
+// // mid:3745kqps, average:3759kqps
+// static void Consumer(KERNEL_NS::LibThreadPool *t)
+// {
+//     TestMqBlock *newEv = NULL;
+//     while (!t->IsDestroy())
+//     {
+//         newEv = NULL;
+//         if(g_Queue->PopFront(newEv))
+//         {
+//             newEv->Release();
+//             ++g_curMsgConsume;
+//         }
+//     }
+// }
 
-// mid:3840kqps, average:3837kqps
-static void Generator2(KERNEL_NS::LibThreadPool *t)
-{
-    // 定时管理
-    KERNEL_NS::SmartPtr<KERNEL_NS::TimerMgr, KERNEL_NS::AutoDelMethods::CustomDelete> timerMgr = KERNEL_NS::TimerMgr::New_TimerMgr();
-    timerMgr.SetClosureDelegate([](void *p){
-        auto ptr = reinterpret_cast<KERNEL_NS::TimerMgr *>(p);
-        KERNEL_NS::TimerMgr::Delete_TimerMgr(ptr);
-    });
-    timerMgr->Launch(NULL);
+// // mid:3840kqps, average:3837kqps
+// static void Generator2(KERNEL_NS::LibThreadPool *t)
+// {
+//     // 定时管理
+//     KERNEL_NS::SmartPtr<KERNEL_NS::TimerMgr, KERNEL_NS::AutoDelMethods::CustomDelete> timerMgr = KERNEL_NS::TimerMgr::New_TimerMgr();
+//     timerMgr.SetClosureDelegate([](void *p){
+//         auto ptr = reinterpret_cast<KERNEL_NS::TimerMgr *>(p);
+//         KERNEL_NS::TimerMgr::Delete_TimerMgr(ptr);
+//     });
+//     timerMgr->Launch(NULL);
 
-    // 内存定时清理
-    KERNEL_NS::SmartPtr<KERNEL_NS::TlsMemoryCleanerComp, KERNEL_NS::AutoDelMethods::CustomDelete> memoryCleaner = KERNEL_NS::TlsMemoryCleanerCompFactory::StaticCreate()->CastTo<KERNEL_NS::TlsMemoryCleanerComp>();
-    memoryCleaner.SetClosureDelegate([](void *p){
-        auto ptr = reinterpret_cast<KERNEL_NS::TlsMemoryCleanerComp *>(p);
-        ptr->Release();
-    });
+//     // 内存定时清理
+//     KERNEL_NS::SmartPtr<KERNEL_NS::TlsMemoryCleanerComp, KERNEL_NS::AutoDelMethods::CustomDelete> memoryCleaner = KERNEL_NS::TlsMemoryCleanerCompFactory::StaticCreate()->CastTo<KERNEL_NS::TlsMemoryCleanerComp>();
+//     memoryCleaner.SetClosureDelegate([](void *p){
+//         auto ptr = reinterpret_cast<KERNEL_NS::TlsMemoryCleanerComp *>(p);
+//         ptr->Release();
+//     });
 
-    // 设置
-    memoryCleaner->SetTimerMgr(timerMgr.AsSelf());
+//     // 设置
+//     memoryCleaner->SetTimerMgr(timerMgr.AsSelf());
 
-    // 启动内存清理
-    do
-    {
-        auto err = memoryCleaner->Init();
-        if(err != Status::Success)
-        {
-            CRYSTAL_TRACE("memory cleaner init fail err:%d", err);
-            break;
-        }
+//     // 启动内存清理
+//     do
+//     {
+//         auto err = memoryCleaner->Init();
+//         if(err != Status::Success)
+//         {
+//             CRYSTAL_TRACE("memory cleaner init fail err:%d", err);
+//             break;
+//         }
 
-        err = memoryCleaner->Start();
-        if(err != Status::Success)
-        {
-            CRYSTAL_TRACE("memory cleaner start fail err:%d", err);
-            break;
-        }
-    } while (false);
+//         err = memoryCleaner->Start();
+//         if(err != Status::Success)
+//         {
+//             CRYSTAL_TRACE("memory cleaner start fail err:%d", err);
+//             break;
+//         }
+//     } while (false);
 
-    while (!t->IsDestroy())
-    {
-        g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
-        ++g_curGenCount;
+//     while (!t->IsDestroy())
+//     {
+//         g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
+//         ++g_curGenCount;
 
-        timerMgr->Drive();
-    }
+//         timerMgr->Drive();
+//     }
 
-    // 关闭
-    memoryCleaner->WillClose();
-    memoryCleaner->Close();
-}
+//     // 关闭
+//     memoryCleaner->WillClose();
+//     memoryCleaner->Close();
+// }
 
 // mid:3661kqps, average:3655kqps
-static void Consumer2(KERNEL_NS::LibThreadPool *t)
-{
-    TestMqBlock *newEv = NULL;
-    while (!t->IsDestroy())
-    {
-        newEv = NULL;
-        if(g_Queue->PopFront(newEv))
-        {
-            newEv->Release();
-            ++g_curMsgConsume;
-        }
-    }
-}
+// static void Consumer2(KERNEL_NS::LibThreadPool *t)
+// {
+//     TestMqBlock *newEv = NULL;
+//     while (!t->IsDestroy())
+//     {
+//         newEv = NULL;
+//         if(g_Queue->PopFront(newEv))
+//         {
+//             newEv->Release();
+//             ++g_curMsgConsume;
+//         }
+//     }
+// }
 
 
-// mid:3602kqps, average:3603kqps
-static void Generator3(KERNEL_NS::LibThreadPool *t)
-{
-    while (!t->IsDestroy())
-    {
-        g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
-        ++g_curGenCount;
-    }
-}
+// // mid:3602kqps, average:3603kqps
+// static void Generator3(KERNEL_NS::LibThreadPool *t)
+// {
+//     while (!t->IsDestroy())
+//     {
+//         g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
+//         ++g_curGenCount;
+//     }
+// }
 
-// mid:3432kqps, average:3426kqps
-static void Consumer3(KERNEL_NS::LibThreadPool *t)
-{
-    TestMqBlock *newEv = NULL;
-    while (!t->IsDestroy())
-    {
-        newEv = NULL;
-        if(g_Queue->PopFront(newEv))
-        {
-            newEv->Release();
-            ++g_curMsgConsume;
-        }
-    }
-}
+// // mid:3432kqps, average:3426kqps
+// static void Consumer3(KERNEL_NS::LibThreadPool *t)
+// {
+//     TestMqBlock *newEv = NULL;
+//     while (!t->IsDestroy())
+//     {
+//         newEv = NULL;
+//         if(g_Queue->PopFront(newEv))
+//         {
+//             newEv->Release();
+//             ++g_curMsgConsume;
+//         }
+//     }
+// }
 
 
 // mid:5358kqps, average:5364kqps
-static void Generator4(KERNEL_NS::LibThreadPool *t)
-{
-    while (!t->IsDestroy())
-    {
-        g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
-        ++g_curGenCount;
-    }
-}
+// static void Generator4(KERNEL_NS::LibThreadPool *t)
+// {
+//     while (!t->IsDestroy())
+//     {
+//         g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
+//         ++g_curGenCount;
+//     }
+// }
 
-// mid:4654kqps, average:4633kqps
-static void Consumer4(KERNEL_NS::LibThreadPool *t)
-{
-    KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT> *lis = KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT>::New_LibList();
-    while (!t->IsDestroy())
-    {
-        g_Queue->SwapQueue(lis);
-        for(auto iter = lis->Begin(); iter;)
-        {
-            auto data = iter->_data;
-            data->Release();
-            iter = lis->Erase(iter);
-            ++g_curMsgConsume;
-        }
-    }
-}
+// // mid:4654kqps, average:4633kqps
+// static void Consumer4(KERNEL_NS::LibThreadPool *t)
+// {
+//     KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT> *lis = KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT>::New_LibList();
+//     while (!t->IsDestroy())
+//     {
+//         g_Queue->SwapQueue(lis);
+//         for(auto iter = lis->Begin(); iter;)
+//         {
+//             auto data = iter->_data;
+//             data->Release();
+//             iter = lis->Erase(iter);
+//             ++g_curMsgConsume;
+//         }
+//     }
+// }
 
-/// 最优解
-// mid:5278kqps, average:5296kqps
-static void Generator5(KERNEL_NS::LibThreadPool *t)
-{
-    // 定时管理
-    KERNEL_NS::SmartPtr<KERNEL_NS::TimerMgr, KERNEL_NS::AutoDelMethods::CustomDelete> timerMgr = KERNEL_NS::TimerMgr::New_TimerMgr();
-    timerMgr.SetClosureDelegate([](void *p){
-        auto ptr = reinterpret_cast<KERNEL_NS::TimerMgr *>(p);
-        KERNEL_NS::TimerMgr::Delete_TimerMgr(ptr);
-    });
-    timerMgr->Launch(NULL);
+// /// 最优解
+// // mid:5278kqps, average:5296kqps
+// static void Generator5(KERNEL_NS::LibThreadPool *t)
+// {
+//     // 定时管理
+//     KERNEL_NS::SmartPtr<KERNEL_NS::TimerMgr, KERNEL_NS::AutoDelMethods::CustomDelete> timerMgr = KERNEL_NS::TimerMgr::New_TimerMgr();
+//     timerMgr.SetClosureDelegate([](void *p){
+//         auto ptr = reinterpret_cast<KERNEL_NS::TimerMgr *>(p);
+//         KERNEL_NS::TimerMgr::Delete_TimerMgr(ptr);
+//     });
+//     timerMgr->Launch(NULL);
 
-    // 内存定时清理
-    KERNEL_NS::SmartPtr<KERNEL_NS::TlsMemoryCleanerComp, KERNEL_NS::AutoDelMethods::CustomDelete> memoryCleaner = KERNEL_NS::TlsMemoryCleanerCompFactory::StaticCreate()->CastTo<KERNEL_NS::TlsMemoryCleanerComp>();
-    memoryCleaner.SetClosureDelegate([](void *p){
-        auto ptr = reinterpret_cast<KERNEL_NS::TlsMemoryCleanerComp *>(p);
-        ptr->Release();
-    });
+//     // 内存定时清理
+//     KERNEL_NS::SmartPtr<KERNEL_NS::TlsMemoryCleanerComp, KERNEL_NS::AutoDelMethods::CustomDelete> memoryCleaner = KERNEL_NS::TlsMemoryCleanerCompFactory::StaticCreate()->CastTo<KERNEL_NS::TlsMemoryCleanerComp>();
+//     memoryCleaner.SetClosureDelegate([](void *p){
+//         auto ptr = reinterpret_cast<KERNEL_NS::TlsMemoryCleanerComp *>(p);
+//         ptr->Release();
+//     });
 
-    // 设置
-    memoryCleaner->SetTimerMgr(timerMgr.AsSelf());
+//     // 设置
+//     memoryCleaner->SetTimerMgr(timerMgr.AsSelf());
 
-    // 启动内存清理
-    do
-    {
-        auto err = memoryCleaner->Init();
-        if(err != Status::Success)
-        {
-            CRYSTAL_TRACE("memory cleaner init fail err:%d", err);
-            break;
-        }
+//     // 启动内存清理
+//     do
+//     {
+//         auto err = memoryCleaner->Init();
+//         if(err != Status::Success)
+//         {
+//             CRYSTAL_TRACE("memory cleaner init fail err:%d", err);
+//             break;
+//         }
 
-        err = memoryCleaner->Start();
-        if(err != Status::Success)
-        {
-            CRYSTAL_TRACE("memory cleaner start fail err:%d", err);
-            break;
-        }
-    } while (false);
+//         err = memoryCleaner->Start();
+//         if(err != Status::Success)
+//         {
+//             CRYSTAL_TRACE("memory cleaner start fail err:%d", err);
+//             break;
+//         }
+//     } while (false);
 
-    while (!t->IsDestroy())
-    {
-        g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
-        ++g_curGenCount;
+//     while (!t->IsDestroy())
+//     {
+//         g_Queue->PushBack(TestMqBlock::NewThreadLocal_TestMqBlock());
+//         ++g_curGenCount;
 
-        timerMgr->Drive();
-    }
+//         timerMgr->Drive();
+//     }
 
-    memoryCleaner->WillClose();
-    memoryCleaner->Close();
-}
+//     memoryCleaner->WillClose();
+//     memoryCleaner->Close();
+// }
 
-/// 最优解
-// mid:4804kqps, average:4796kqps
-static void Consumer5(KERNEL_NS::LibThreadPool *t)
-{
-    KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT> *lis = KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT>::New_LibList();
-    TestMqBlock *newEv = NULL;
-    while (!t->IsDestroy())
-    {
-        newEv = NULL;
-        g_Queue->SwapQueue(lis);
-        for(auto iter = lis->Begin(); iter;)
-        {
-            auto data = iter->_data;
-            data->Release();
-            iter = lis->Erase(iter);
-            ++g_curMsgConsume;
-        }
-    }
-}
+// /// 最优解
+// // mid:4804kqps, average:4796kqps
+// static void Consumer5(KERNEL_NS::LibThreadPool *t)
+// {
+//     KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT> *lis = KERNEL_NS::LibList<KERNEL_NS::PollerEvent *, KERNEL_NS::_Build::MT>::New_LibList();
+//     TestMqBlock *newEv = NULL;
+//     while (!t->IsDestroy())
+//     {
+//         newEv = NULL;
+//         g_Queue->SwapQueue(lis);
+//         for(auto iter = lis->Begin(); iter;)
+//         {
+//             auto data = iter->_data;
+//             data->Release();
+//             iter = lis->Erase(iter);
+//             ++g_curMsgConsume;
+//         }
+//     }
+//     UNUSED(newEv);
+// }
 
 
 // >= 1000w qps
