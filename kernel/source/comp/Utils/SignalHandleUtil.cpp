532addeca0a1ae83d50bf670301d078f2b10bb54
ericyonng <120453674@qq.com>
2024-09-02 22:26:27 +0800

    linux 下捕获异常不需要打堆栈

--- kernel/source/comp/Utils/SignalHandleUtil.cpp
+++ kernel/source/comp/Utils/SignalHandleUtil.cpp
@@ -46,8 +46,14 @@ extern "C"
         // 1.打印堆栈
         if(LIKELY(g_Log))
         {
+        #if CRYSTAL_TARGET_PLATFORM_WINDOWS
+            g_Log->Info(LOGFMT_NON_OBJ_TAG(KERNEL_NS::SignalHandleUtil, "catch signal:%d, %s, process id:%d thread id:%llu, main thread id:%llu stack backtrace:%s")
+                , signalNo, KERNEL_NS::SignalHandleUtil::SignalToString(signalNo).c_str(), processId, KERNEL_NS::SystemUtil::GetCurrentThreadId(), mainThreadId,
+                 KERNEL_NS::BackTraceUtil::CrystalCaptureStackBackTrace().c_str());
+        #else
             g_Log->Info(LOGFMT_NON_OBJ_TAG(KERNEL_NS::SignalHandleUtil, "catch signal:%d, %s, process id:%d thread id:%llu, main thread id:%llu")
                 , signalNo, KERNEL_NS::SignalHandleUtil::SignalToString(signalNo).c_str(), processId, KERNEL_NS::SystemUtil::GetCurrentThreadId(), mainThreadId);
+        #endif
         }
 
         if(signalNo > 0)
