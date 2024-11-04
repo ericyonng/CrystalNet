f1539d73bac6deb0e5a771569abbbaf7da820d8c
ericyonng <120453674@qq.com>
2024-11-04 11:53:31 +0800

    fix 编译问题

--- kernel/include/kernel/comp/Coroutines/CoTask.h
+++ kernel/include/kernel/comp/Coroutines/CoTask.h
@@ -73,7 +73,12 @@ public:
     CoTask& operator=(CoTask&& other)
     {
         Destroy();
-        _handle = std::exchange(other._handle, {});
+
+        #if CRYSTAL_PLATFORM_WINDOWS
+        _handle = std::exchange(other._handle, NULL);
+        #else
+        _handle = std::exchange(other._handle, std::nullptr_t);
+        #endif
         _disableSuspend = std::exchange(other._disableSuspend, false);
         return *this;
     }
@@ -369,7 +374,11 @@ private:
     void Destroy() 
     {
         // 协程必须真正结束才能销毁
+        #if CRYSTAL_PLATFORM_WINDOWS
         if (auto handle = std::exchange(_handle, NULL))
+        #else
+        if (auto handle = std::exchange(_handle, std::nullptr_t))
+        #endif
         {
             auto &promise = handle.promise();
             promise.SetSelfDestory(true);
