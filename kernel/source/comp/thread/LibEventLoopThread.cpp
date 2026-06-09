7594bc9a6a76ba7f640695b19ab7748e8bc678ad
ericyonng <120453674@qq.com>
2026-06-09 16:44:54 +0800

    优化线程关闭

--- kernel/source/comp/thread/LibEventLoopThread.cpp
+++ kernel/source/comp/thread/LibEventLoopThread.cpp
@@ -108,15 +108,18 @@ bool LibEventLoopThread::HalfClose()
 void LibEventLoopThread::FinishClose()
 {
     auto poller = _thread->GetPollerNoAsync();
-    for (Int32 idx = 0; idx < 10; ++idx)
+    if (!poller)
     {
-        KERNEL_NS::SystemUtil::ThreadSleep(1000);
-        poller = _thread->GetPollerNoAsync();
-        if (g_Log && g_Log->IsEnable(LogLevel::Warn))
-            g_Log->Warn(LOGFMT_OBJ_TAG("thread:%s, poller not ready"), _thread->ToString().c_str());
+        for (Int32 idx = 0; idx < 2; ++idx)
+        {
+            KERNEL_NS::SystemUtil::ThreadSleep(1000);
+            poller = _thread->GetPollerNoAsync();
+            if (g_Log && g_Log->IsEnable(LogLevel::Warn))
+                g_Log->Warn(LOGFMT_OBJ_TAG("thread:%s, poller not ready"), _thread->ToString().c_str());
 
-        if (poller)
-            break;
+            if (poller)
+                break;
+        }
     }
 
     // 退出事件循环
